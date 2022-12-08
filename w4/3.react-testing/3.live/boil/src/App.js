062d86249fd5ec9a3d1c89b62f63c131aea6f11c
Ritesh Firodiya <ritesh.firodiya@gmail.com>
2022-12-08 16:34:13 +0530

    added boiler

---
+++ w4/3.react-testing/3.live/boil/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "./components/Button/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <Button />
+    </div>
+  );
+}
+
+export default App;
