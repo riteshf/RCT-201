f375cacb1eea929cb66f18c53388cc4fa2e8215e
Ritesh Firodiya <ritesh.firodiya@gmail.com>
2023-01-13 20:31:40 +0530

    added data

--- w4/4.psc/ref/components/Cart.tsx
+++ w4/4.psc/ref/components/Cart.tsx
@@ -1,22 +1,68 @@
 import React from "react";
-import { Button } from "@chakra-ui/react";
+import {
+  Button,
+  useDisclosure,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  Box,
+  DrawerFooter,
+} from "@chakra-ui/react";
+import { useSelector } from "react-redux";
 import { BsBag } from "react-icons/bs";
 
+import { RootState } from "../redux/store";
+
 const Cart = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = React.useRef(null);
   return (
-    <Button
-      variant="outline"
-      colorScheme="gray"
-      display="flex"
-      gap={2}
-      alignItems="center"
-      borderWidth={2}
-      borderColor="gray.800"
-      borderRadius="full"
-    >
-      <BsBag />
-      {"TODO"}
-    </Button>
+    <Box>
+      <Button
+        variant="outline"
+        colorScheme="gray"
+        display="flex"
+        gap={2}
+        alignItems="center"
+        borderWidth={2}
+        borderColor="gray.800"
+        borderRadius="full"
+        ref={btnRef}
+        onClick={onOpen}
+      >
+        <BsBag />
+        {"0"}
+      </Button>
+      <Drawer
+        isOpen={isOpen}
+        onClose={onClose}
+        finalFocusRef={btnRef}
+        placement="right"
+        size="md"
+      >
+        <DrawerOverlay>
+          <DrawerContent borderLeftRadius="24">
+            <DrawerCloseButton mt={4} />
+            <DrawerHeader mt={4}>Shopping Cart</DrawerHeader>
+            <DrawerBody>TODO</DrawerBody>
+            <DrawerFooter>
+              <Button
+                size="lg"
+                w="full"
+                colorScheme="blackAlpha"
+                onClick={onClose}
+                mb={12}
+              >
+                Checkout
+              </Button>
+            </DrawerFooter>
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
+    </Box>
   );
 };
 
